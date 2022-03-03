871e3979ea6dd274b9d380b0023bda1167065d48
Jegesmaci583 <hunraindow@gmail.com>
2022-03-03 15:07:36 +0100

    táblázat nélküli

---
+++ src/bmi_index/BMI_Index.java
@@ -0,0 +1,27 @@
+package bmi_index;
+public class BMI_Index {
+    public static void main(String[] args) {
+        cim("");
+        int tomeg = beker("");
+        int magassag = beker("");
+        double bmiIndex = bmi(tomeg, magassag);
+        ertekeles();
+    }
+
+    static void cim(String szoveg) {
+        System.out.println(szoveg);
+    }
+
+    static int beker(String string) {
+        int 
+    }
+
+    static double bmi(int tomeg, int magassag) {
+        
+    }
+
+    static void ertekeles() {
+        
+    }
+    
+}
