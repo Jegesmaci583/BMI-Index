e8921bc4612a7713b640b02be721b31eab440172
Jegesmaci583 <hunraindow@gmail.com>
2022-03-03 15:16:58 +0100

    táblázat nélküliv2

--- src/bmi_index/BMI_Index.java
+++ src/bmi_index/BMI_Index.java
@@ -1,4 +1,7 @@
 package bmi_index;
+
+import java.util.Locale;
+
 public class BMI_Index {
     public static void main(String[] args) {
         cim("");
@@ -12,16 +15,32 @@ public class BMI_Index {
         System.out.println(szoveg);
     }
 
-    static int beker(String string) {
-        int 
+    static int beker(String kerdes) {
+        System.out.print(kerdes);
+        
+        Scanner sc = new Scanner
+        int szam = -1;
+        boolean jo = false;
+        while(!jo){
+            szam =  sc.nextInt();
+            jo = szam >= 100 && szam <= 200;
+            if(!jo){
+                System.out.println("Nem jó,q újra");
+                System.out.println(kerdes);
+            }
+    }
+        return 0;
     }
 
     static double bmi(int tomeg, int magassag) {
-        
+        return 0.0;
     }
 
-    static void ertekeles() {
-        
+    static void ertekeles(int tomeg, int magassag, double bmiIndex) {
+        System.out.printf("Ön %d cm magas és %d kg tomegű," magassag, tomeg);
+        System.out.printf(Locale.ENGLISH, " Így BMI indexe %");
+        String testalkat = "túlsúlyos";
+        System.out.printf("Tehát ön %s testalkatú\n", testalkat);
     }
     
 }
